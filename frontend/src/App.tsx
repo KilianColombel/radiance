7788c6028a70443c49c56168caef961e998fa324
kilian.colombel <kilian.colombel@ensiie.eu>
2025-08-03 21:03:10 +0200

    basic layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,14 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      initial commit :p
-    </>
+    <div className='app-container'>
+      <div>search bar</div>
+      <div className='center-container'>
+        <div className='side-panel'>side panel</div>
+        <div className='main-container'>main div</div>
+      </div>
+      <div >player</div>
+    </div>
   )
 }
 

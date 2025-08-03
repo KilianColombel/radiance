48427a0a339e0036fc46cf8f61d0ca3d2bd071de
kilian.colombel <kilian.colombel@ensiie.eu>
2025-08-04 00:32:00 +0200

    added player layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,15 +8,41 @@ function App() {
     <div className='app-container'>
       <div>search bar</div>
         <PanelGroup className='center-container' direction="horizontal">
-          <Panel className='side-panel' defaultSize={25} minSize={15} maxSize={35}>
-            right
-          </Panel>
-          <PanelResizeHandle style={{width: "5px"}}/>
-          <Panel className="main-container" defaultSize={75} minSize={65}>
+          <Panel className='side-panel' collapsible={true} collapsedSize={4} defaultSize={25} minSize={15} maxSize={35}>
             left
           </Panel>
+          <PanelResizeHandle className='resize-handle' style={{width: "6px"}}/>
+          <Panel className="main-container" defaultSize={75} minSize={65}>
+            middle
+          </Panel>
         </PanelGroup>
-      <div >player</div>
+      <div className='bottom-player'>
+        <audio src=""></audio>
+        <div className='left-infos'>
+          <img src="https://ia800305.us.archive.org/15/items/mbid-f1dd454a-6705-4828-97d8-57a423451c06/mbid-f1dd454a-6705-4828-97d8-57a423451c06-35311231946_thumb500.jpg" alt="" />
+          <div className='item-infos'>
+            <div className='title-name'>Radiance</div>
+            <div className='artist-name'>Tim Hecker</div>
+          </div>
+          <i className="bi bi-star"></i>
+        </div>
+        <div className='middle-controls'>
+          <div className='controls-container'>
+            <i className="bi bi-skip-start"></i>
+            <i className="bi bi-play"></i>
+            <i className="bi bi-skip-end"></i>
+          </div>
+          <div className='progress-slider'>
+            <div>0:00</div>
+            <input type="range" />
+            <div>6:66</div>
+          </div>
+        </div>
+        <div className='right-controls'>
+          <input type="range" />
+          <i className="bi bi-plus-square"></i>
+        </div>
+      </div>
     </div>
   )
 }

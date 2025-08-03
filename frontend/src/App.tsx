2b1b9563798b34d97c2f7d420b231a67889892c6
kilian.colombel <kilian.colombel@ensiie.eu>
2025-08-03 22:48:52 +0200

    added resizable left panel with react-resizable-panel dependency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import './App.css'
 
 function App() {
@@ -6,10 +7,15 @@ function App() {
   return (
     <div className='app-container'>
       <div>search bar</div>
-      <div className='center-container'>
-        <div className='side-panel'>side panel</div>
-        <div className='main-container'>main div</div>
-      </div>
+        <PanelGroup className='center-container' direction="horizontal">
+          <Panel className='side-panel' defaultSize={25} minSize={15} maxSize={35}>
+            right
+          </Panel>
+          <PanelResizeHandle style={{width: "5px"}}/>
+          <Panel className="main-container" defaultSize={75} minSize={65}>
+            left
+          </Panel>
+        </PanelGroup>
       <div >player</div>
     </div>
   )
